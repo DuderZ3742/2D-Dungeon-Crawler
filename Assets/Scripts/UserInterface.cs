dcf9f61c1ee9945cd1a3678c1dbb08eb3e44c61a
DuderZ3742 <118741814+DuderZ3742@users.noreply.github.com>
2022-11-22 16:57:27 +0000

    MiniMapAdded
    For Testing The Levels

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -6,8 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class UserInterface : MonoBehaviour
 {
+    public GameObject miniMap;
+
+    public GameObject miniMapCam;
+
     public void ReloadScene()
     {
         SceneManager.LoadScene("MainGame");     //When the Reload Scene button is pressed in game, another random level will load
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            miniMap.SetActive(true);
+            miniMapCam.SetActive(true);
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            miniMap.SetActive(false);
+            miniMapCam.SetActive(false);
+        }
     }
 }
