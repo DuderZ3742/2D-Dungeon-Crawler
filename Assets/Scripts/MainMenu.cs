db64a0e2eb0d0fcacfe3b352e36deec423aa7ac3
DuderZ3742 <118741814+DuderZ3742@users.noreply.github.com>
2022-12-06 17:01:40 +0000

    STUFF

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MainGame");     //When the Play button is pressed, the game will load
+    }
+}
